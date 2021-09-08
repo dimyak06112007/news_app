dbf20dc8b3b06abc0a1083592cb243492c844dcf
dimyak06112007 <dimyak0611@gmail.com>
2021-09-08 06:09:57 +0300

    fine, but doesn't display images in browser

--- lib/components/customListTile.dart
+++ lib/components/customListTile.dart
@@ -2,6 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
 import 'package:news_app/pages/articles_details_page.dart';
 
+// import 'dart:html' as html;
+//
+// @override
+// final String src = '';
+// final html.IFrameElement _iframeElement = html.IFrameElement();
+//
+// void _setup(String src, num width, num height){
+//   final src = src;
+//   ui.platformViewRegistry.registerViewFactory('iframe-$src', (int viewId){
+//     final element =_iframeElement
+//     ..style.border = '0'
+//     ..allowFullscreen = widget.wedAllowFullScreen
+//     ..height = height.toInt().toString()
+//     ..width = width.toInt().toString();
+//     if (src !=null){
+//       String _src = src;
+//       if()
+//     }
+//   })
+// }
+Widget Img(article) => Image.network(
+      article.urlToImage,
+      key: UniqueKey(),
+      fit: BoxFit.cover,
+      width: double.infinity,
+      height: 200,
+      loadingBuilder: (BuildContext context, Widget child,
+          ImageChunkEvent? loadingProgress) {
+        if (loadingProgress == null) {
+          return child;
+        } else {
+          return Center(
+            child: Container(
+              width: double.infinity,
+              height: 200,
+              child: CircularProgressIndicator(
+                color: Colors.lightBlueAccent,
+                value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes!
+                    : null,
+              ),
+            ),
+          );
+        }
+      },
+    );
+
 Widget customListTile(Article article, BuildContext context) {
   return InkWell(
     onTap: () {
@@ -33,30 +81,7 @@ Widget customListTile(Article article, BuildContext context) {
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(9),
-            child: Image.network(
-              article.urlToImage,
-              key: UniqueKey(),
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: 200,
-              loadingBuilder: (BuildContext context, Widget child,
-                  ImageChunkEvent? loadingProgress) {
-                if (loadingProgress == null) return child;
-                return Center(
-                  child: Container(
-                    width: double.infinity,
-                    height: 200,
-                    child: CircularProgressIndicator(
-                      color: Colors.lightBlueAccent,
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded /
-                              loadingProgress.expectedTotalBytes!
-                          : null,
-                    ),
-                  ),
-                );
-              },
-            ),
+            child: Img(article),
           ),
           SizedBox(
             height: 8.0,
